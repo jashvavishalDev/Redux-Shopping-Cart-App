80c77a7926d56600c429e0b0bd0a3226076a4347
jashvavishalDev <informjashvavishal@gmail.com>
2022-06-27 14:33:02 +0530

    Firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { fetchData, sendCartData } from "./store/cart-actions";
+// import { uiActions } from "./store/ui-slice";
 
+let isFirstRender =true;
 function App() {
-  const isLoggedIn= useSelector((state)=> state.auth.isLoggedIn)
-  console.log(isLoggedIn);
-  const cartItems = useSelector((state)=> state.cart.itemsList);
-  console.log(cartItems);
+  const dispatch= useDispatch();
+  const notification= useSelector(state=>state.ui.notification);
+  const cart= useSelector(state=> state.cart);
+
+  const isLoggedIn= useSelector((state)=> state.auth.isLoggedIn);
+
+   useEffect(()=>{
+      dispatch(fetchData());
+   },[dispatch])
+
+  useEffect(()=>{
+
+    if(isFirstRender){
+      isFirstRender=false;
+      return;
+    }
+    if(cart.changed){
+         dispatch(sendCartData(cart));
+
+    }
+
+    // const sendRequest= async ()=>{
+    //   //Send State as Sending Request
+    //   dispatch(uiActions.showNotification({
+    //     open: true,
+    //     message: "Sending Request",
+    //     type: 'warning'
+    //   }))
+    //   const res=  await fetch('https://redux-http-1ade3-default-rtdb.firebaseio.com/cartItems.json',{
+    //     method: "PUT",
+    //     body: JSON.stringify(cart)
+    //    });
+    //    const data= await res.json();
+    //    //Send State as Request is succesful
+    //    dispatch(uiActions.showNotification({
+    //     open: true,
+    //     message: "Sent Request to DaaBase succesfully",
+    //     type: 'success'
+    //   }))
+    // };
+    //  sendRequest().catch(err=>{
+        //Send state as Error
+        // dispatch(uiActions.showNotification({
+        //   open: true,
+        //   message: "Sending Request failed",
+        //   type: 'error'
+        // }))
+    //  });
+   },[cart, dispatch]);
+
   return (
     <div className="App">
+     {notification && <Notification type={notification.type} message={notification.message}/>}
       { !isLoggedIn && <Auth />}
       { isLoggedIn && <Layout />}
     </div>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,14 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit"; 
+// import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
         itemsList: [],
         totalQuantity: 0,
-        showCart: false
+        showCart: false,
+        changed: false,
     },
     reducers: {
+        replaceData(state, action) {
+                state.totalQuantity =action.payload.totalPrice;
+                state.itemsList= action.payload.itemsList;
+
+         },
             addToCart(state, action) { 
+                state.changed= true;
                 const newItem= action.payload;
                 // to check if item already available
                 const existingItem =state.itemsList.find((item)=> item.id===newItem.id);
@@ -28,6 +36,7 @@ const cartSlice = createSlice({
                 }
              },
             removeFromCart(state, action) {
+                state.changed = true;
                 const id = action.payload;
                 const existingItem= state.itemsList.find(item=> item.id === id);
                 if(existingItem.quantity === 1){
@@ -42,8 +51,8 @@ const cartSlice = createSlice({
                 state.showCart = !state.showCart;
             },
     }
-})
-
+});
+ 
 export const cartActions= cartSlice.actions;
 
 export default cartSlice; 
